c0065f80f84c0f167faedccdede7bccd3b9debd1
Soumik Das <79033574+FalconEthics@users.noreply.github.com>
2023-03-12 12:45:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 //create an implementaion of chatgpt of openai for discord
-require('dotenv').config();
+require("dotenv").config();
 
 // Prepare to connect discord api
-const{ Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-
-] });
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
 //prepare to connect openai api
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_KEY
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
 //check for when message is sent
-client.on('messageCreate', async function(message){
-    try{
-        if(message.author.bot) return;
+client.on("messageCreate", async function (message) {
+  try {
+    if (message.author.bot) return;
 
-        if(message.channelId == '1082554120612876308'){
-            const response = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: `Hey give me a response to this message: ${message.content}`,
-                temperature: 0.5,
-                max_tokens: 60,
-                top_p: 1.0,
-                frequency_penalty: 0.5,
-                presence_penalty: 0.0,
-              });
-
-            message.reply(`${response.data.choices[0].text}`)
-        }
-    } catch(error){
-        console.log(error);
+    if (message.channelId == "1082554120612876308") {
+      message.reply("w8 processing... your query");
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Hey give me a response to this message: ${message.content}`,
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+      });
+      message.reply(`${response.data.choices[0].text}`);
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //Login to discord
 client.login(process.env.DISCORD_TOKEN);
-console.log('ChatGPT is running');
+console.log("ChatGPT is running");
